53733923d57bd0b014c5c47b8a24c58be2497ed8
dhinesh1704 <dhaneshraja41998@gmail.com>
2022-10-01 11:57:30 +0530

    project1

--- Example1.java
+++ Example1.java
@@ -3,7 +3,7 @@ package For2;
 public class Example1 {
 	public static void main(String[]args) {
 		String[] names=new String[4];
-		names[0]="POCO";
+		names[0]="Samsung";
 		names[1]="MI";
 		names[2]="realmi";
 		names[3]="nokia";
